cb3758606c91eee87f1dbbe7a56a7f4b5a7c819d
Paula Haidamac <haidamacpaula@gmail.com>
2026-01-17 17:11:11 +0200

    Added navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SpinnerComponent } from "./services/spinner/spinner.component";
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SpinnerComponent],
+  imports: [
+    RouterOutlet, 
+    SpinnerComponent, 
+    NavbarComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
